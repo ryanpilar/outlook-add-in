162ab8f580de878d4d3186e5a7159cf949ace421
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2025-09-25 22:21:11 -0600

    Revert "Open task pane directly from ribbon button"

--- ui/src/taskpane/components/App.tsx
+++ ui/src/taskpane/components/App.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
 import TextInsertion from "./TextInsertion";
 import { makeStyles } from "@fluentui/react-components";
+import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 import { sendText } from "../taskpane";
 
 interface AppProps {
@@ -16,10 +18,28 @@ const useStyles = makeStyles({
 
 const App: React.FC<AppProps> = (props: AppProps) => {
   const styles = useStyles();
+  // The list items are static and won't change at runtime,
+  // so this should be an ordinary const, not a part of state.
+  const listItems: HeroListItem[] = [
+    {
+      icon: <Ribbon24Regular />,
+      primaryText: "Achieve more with Office integration",
+    },
+    {
+      icon: <LockOpen24Regular />,
+      primaryText: "Unlock features and functionality",
+    },
+    {
+      icon: <DesignIdeas24Regular />,
+      primaryText: "Create and visualize like a pro",
+    },
+  ];
+
   return (
     <div className={styles.root}>
       <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
-      <TextInsertion sendText={sendText} />
+      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
+        <TextInsertion sendText={sendText} />
     </div>
   );
 };
